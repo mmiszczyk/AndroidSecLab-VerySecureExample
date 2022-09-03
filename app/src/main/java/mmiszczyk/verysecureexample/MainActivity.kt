cd0273cc01a05377ade0777665a0beeace453fed
Maciej Miszczyk <maciejmmiszczyk@gmail.com>
2022-09-03 14:30:55 +0200

    Initial commit

---
+++ app/src/main/java/mmiszczyk/verysecureexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package mmiszczyk.verysecureexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Thread {
+            val host = "example.org"
+            val pin = CertificatePinner
+                .Builder()
+                .add(host, "sha256/S4kZuhQQ1DPcMOCYFQXD0gG+UW0zmyVx6roNWpRl65I=")
+                .add(host, "sha256/RQeZkB42znUfsDIIFWIRiYEcKl7nHwNFwWCrnMMJbVc=")
+                .add(host, "sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=")
+                .build()
+            val c = OkHttpClient.Builder().certificatePinner(pin).build()
+            val r = Request.Builder().url("https://$host").build()
+            c.newCall(r).execute()
+        }.start()
+    }
+}
